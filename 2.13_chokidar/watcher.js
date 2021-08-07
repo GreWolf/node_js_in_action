5566b8552c8cab31cee26e0507af50272c811b7b
grey_wolf <grey_wolf.88@mail.ru>
2021-08-07 23:15:45 +0300

    watcher

--- 2.13_chokidar/watcher.js
+++ 2.13_chokidar/watcher.js
@@ -11,21 +11,35 @@ var watcher = chokidar.watch(watchDir, {
 var log = console.log.bind(console);
 // Add event listeners.
 watcher
-    .on('add', function (path) { return log("File " + path + " has been added"); })
-    // .on('change', path => log(`File ${path} has been changed`))
-    .on('unlink', function (path) { return log("File " + path + " has been removed"); });
+    .on('add', function (path) { return log(getCurrDateStr() + " File " + path + " has been added"); })
+    .on('unlink', function (path) { return log(getCurrDateStr() + " File " + path + " has been removed"); });
 // More possible events.
 watcher
-    .on('addDir', function (path) { return log("Directory " + path + " has been added"); })
-    .on('unlinkDir', function (path) { return log("Directory " + path + " has been removed"); })
-    .on('error', function (error) { return log("Watcher error: " + error); })
-    .on('ready', function () { return log('Initial scan complete. Ready for changes'); })
+    .on('addDir', function (path) { return log(getCurrDateStr() + " Directory " + path + " has been added"); })
+    .on('unlinkDir', function (path) { return log(getCurrDateStr() + " Directory " + path + " has been removed"); })
+    .on('error', function (error) { return log(getCurrDateStr() + " Watcher error: " + error); })
+    .on('ready', function () { return log(getCurrDateStr() + " Initial scan complete. Ready for changes"); })
     .on('raw', function (event, path, details) {
-    log('Raw event info:', event, path, details);
+    log(getCurrDateStr() + " Raw event info:", event, path, details);
 });
 // 'add', 'addDir' and 'change' events also receive stat() results as second
 // argument when available: https://nodejs.org/api/fs.html#fs_class_fs_stats
 watcher.on('change', function (path, stats) {
-    if (stats)
-        console.log("File " + path + " changed size to " + stats.size);
+    if (stats) {
+        console.log(getCurrDateStr() + " File " + path + " changed size to " + stats.size);
+    }
+    else
+        log(getCurrDateStr() + " File " + path + " has been changed");
 });
+function getCurrDateStr() {
+    return dateToString(new Date());
+}
+function dateToString(date) {
+    var YY = date.getFullYear();
+    var MM = String(date.getMonth() + 1).padStart(2, '0');
+    var DD = String(date.getDay() + 1).padStart(2, '0');
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+    return YY + "." + MM + "." + DD + " " + hh + "." + mm + "." + ss;
+}
--- 2.13_chokidar/watcher.ts
+++ 2.13_chokidar/watcher.ts
@@ -3,30 +3,50 @@ import * as chokidar from 'chokidar';
 const watchDir: string = './2.13_chokidar/watchDir'
 // One-liner for current directory
 const watcher = chokidar.watch(watchDir, {
-  ignored: /(^|[\/\\])\../, // ignore dotfiles
-  persistent: true,
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+    persistent: true,
 })
 
 // Something to use when events are received.
 const log = console.log.bind(console);
 // Add event listeners.
 watcher
-  .on('add', path => log(`File ${path} has been added`))
-  // .on('change', path => log(`File ${path} has been changed`))
-  .on('unlink', path => log(`File ${path} has been removed`));
+    .on('add', path => log(`${getCurrDateStr()} File ${path} has been added`))
+    .on('unlink', path => log(`${getCurrDateStr()} File ${path} has been removed`));
 
 // More possible events.
 watcher
-  .on('addDir', path => log(`Directory ${path} has been added`))
-  .on('unlinkDir', path => log(`Directory ${path} has been removed`))
-  .on('error', error => log(`Watcher error: ${error}`))
-  .on('ready', () => log('Initial scan complete. Ready for changes'))
-  .on('raw', (event, path, details) => { // internal
-    log('Raw event info:', event, path, details);
-  });
+    .on('addDir', path => log(`${getCurrDateStr()} Directory ${path} has been added`))
+    .on('unlinkDir', path => log(`${getCurrDateStr()} Directory ${path} has been removed`))
+    .on('error', error => log(`${getCurrDateStr()} Watcher error: ${error}`))
+    .on('ready', () => log(`${getCurrDateStr()} Initial scan complete. Ready for changes`))
+    .on('raw', (event, path, details) => { // internal
+        log(`${getCurrDateStr()} Raw event info:`, event, path, details);
+    });
 
 // 'add', 'addDir' and 'change' events also receive stat() results as second
 // argument when available: https://nodejs.org/api/fs.html#fs_class_fs_stats
 watcher.on('change', (path, stats) => {
-  if (stats) console.log(`File ${path} changed size to ${stats.size}`);
+    if (stats) {
+        console.log(`${getCurrDateStr()} File ${path} changed size to ${stats.size}`)
+    }
+    else log(`${getCurrDateStr()} File ${path} has been changed`)
+
 });
+
+
+function getCurrDateStr(): string {
+    return dateToString(new Date())
+}
+
+function dateToString(date: Date): string {
+    let YY = date.getFullYear()
+    let MM = String(date.getMonth() + 1).padStart(2, '0')
+    let DD = String(date.getDay() + 1).padStart(2, '0')
+
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+
+    return `${YY}.${MM}.${DD} ${hh}.${mm}.${ss}`
+}
